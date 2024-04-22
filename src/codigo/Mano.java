16aec27c8c8fdd6485d224cdafa080f9a73ac14d
Juan Francisco Zamora Agudo <pjuanzamora@gmail.com>
2024-04-22 19:50:06 +0200

    Creando método para comparar manos

--- src/codigo/Mano.java
+++ src/codigo/Mano.java
@@ -15,6 +15,11 @@ public class Mano {
 	public void setJugada(String jugada) {
 		this.jugada = jugada;
 	}
+
+	//Nos falta aquí un método que se encargue comparar si una jugada gana a otra
+	public boolean compararJugadas(Mano mano){
+		//Te toca hacerlo
+	}
 }
 
 // voy a probar con esto a ver que tal
