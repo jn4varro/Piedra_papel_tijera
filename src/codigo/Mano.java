258506a927d32ebb8ee3ccd72e5caaa3dac6ee36
jn4varro <jnavarro.artist@gmail.com>
2024-04-22 17:58:09 +0200

    Clase mano mejorada

--- src/codigo/Mano.java
+++ src/codigo/Mano.java
@@ -1,5 +1,20 @@
 package codigo;
 
 public class Mano {
+	private String jugada;
+	
+	//constructor
+	public Mano(String jugada) {
+		this.setJugada(jugada);
+	}
+	//getters y setters
+	public String getJugada() {
+		return jugada;
+	}
 
+	public void setJugada(String jugada) {
+		this.jugada = jugada;
+	}
 }
+
+// voy a probar con esto a ver que tal
