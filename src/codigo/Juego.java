c416a86f0f0187ecc274dd430d1902ca50c6b356
Juan Francisco <pjuanzamora@gmail.com>
2024-04-23 23:10:43 +0200

    Create Juego.java

---
+++ src/codigo/Juego.java
@@ -0,0 +1,27 @@
+package codigo;
+
+//La clase enfrentara dos jugadores guardando el resultado de cada "mano"
+// gana el jugador que mas manos gane
+// Al final se tiene que poder imprimir por pantalla el ganador
+// Añade los atributos que consideres necesarios
+public class Juego {
+
+
+  private Jugador jugador1;
+  private Jugador jugador2;
+  
+
+//Crear constructor que inicialice los dos jugadores
+  // 
+  public Juego....
+
+  //Metodo que se llama jugarPartida()
+  // declaaralo como consideres
+   public .....
+
+  //Imprimir resultado partida junto con los datos del juego
+  public String mostrarResultado(){
+    
+  }
+  
+}
